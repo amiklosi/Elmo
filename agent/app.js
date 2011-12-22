84a77aab3cc5b41fb570a7cf107a80067fb75d65
Jozef Dransfield <Jozef.Dransfield@me.com>
2011-12-22 19:52:03 +0000

    Seeing if adding a server makes the agent work

--- agent/app.js
+++ agent/app.js
@@ -7,3 +7,9 @@ var agent = require("./lib/agent");
 var myAgent = new agent.Agent(new agent.AgentEventEmitter(), new agent.AgentEventProcessor())
 
 console.log("Agent Started!");
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(8080, "127.0.0.1");
