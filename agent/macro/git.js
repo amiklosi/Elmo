ce7b8924e5099adb6431c71f8397005cf7ef0ef4
Jozef Dransfield <Jozef.Dransfield@me.com>
2012-01-10 23:55:47 +0000

    can now run js tasks and macros

--- agent/macro/git.js
+++ agent/macro/git.js
@@ -6,7 +6,7 @@ module.exports = function(step, directory, callback) {
     output += " Project Name: "+ projectName(step.repo);
 
     fs.stat(directory+"/"+projectName(step.repo), function(error, stats) {
-        if (stats.isDirectory()) {
+        if (stats && stats.isDirectory()) {
             var command = "git pull";
             exec(command, {cwd:directory+"/"+projectName(step.repo), env:process.env}, function (error, stdout, stderr) {
                 callback(error, command + "\n" + stdout, stderr);
